23d936c08886646b9c8e8bbd4a9791879b8cfdb2
unknown <elprro0019th@gmail.com>
2025-01-25 11:35:16 -0600

    mejoras

--- js/login.js
+++ js/login.js
@@ -12,23 +12,23 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     if (username === "Angel" && password === "22411070200076") {
         localStorage.setItem("loggedUser", "Angel");
         alert("Inicio de sesión exitoso.");
-        window.location.replace("Archivos");
+        window.location.replace("Descanso");
     } else if (username === "Jose" && password === "22411070200217") {
         localStorage.setItem("loggedUser", "Jose");
         alert("Inicio de sesión exitoso.");
-        window.location.replace("Archivos");
+        window.location.replace("Descanso");
     } else if (username === "Fernanda" && password === "22411070200176") {
         localStorage.setItem("loggedUser", "Fernanda");
         alert("Inicio de sesión exitoso.");
-        window.location.replace("Archivos");
+        window.location.replace("Descanso");
     } else if (username === "Adrian" && password === "1234") {
         localStorage.setItem("loggedUser", "Adrian");
         alert("Inicio de sesión exitoso.");
-        window.location.replace("Archivos");
+        window.location.replace("Descanso");
     } else if (username === "Veronica" && password === "23411070200143") {
         localStorage.setItem("loggedUser", "Veronica");
         alert("Inicio de sesión exitoso.");
-        window.location.replace("Archivos");
+        window.location.replace("Descanso");
     } else {
         console.log("Credenciales incorrectas.");
         alert("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
