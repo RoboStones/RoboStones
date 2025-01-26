48e0fdaa06a4aefc8d881a72909091eb726428b8
unknown <elprro0019th@gmail.com>
2025-01-26 11:05:27 -0600

    Actualizacion css y js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
-    // Mostrar el botón de regresar
-    function showBackButton(event) {
-        const backButton = document.getElementById('backButton');
-        backButton.classList.remove('hidden');
-    }
+//Cerrar segundo apartado de las imagenes
 
-    // Ocultar el botón de regresar
-    function hideBackButton() {
-        const backButton = document.getElementById('backButton');
-        backButton.classList.add('hidden');
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.redesS').forEach(section => {
+        const closeButton = section.querySelector('.close-btn');
+        const content = section.querySelector('.redesS__content');
+
+        if (closeButton && content) {
+            const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+            
+            section.addEventListener('click', (event) => {
+                if (event.target !== closeButton) {
+                    content.style.transform = 'rotateX(0deg)';
+                }
+            });
+
+            closeButton.addEventListener('click', (event) => {
+                event.stopPropagation(); 
+                content.style.transform = 'rotateX(-90deg)'; 
+            });
+
+            if (!isMobile) {
+                window.addEventListener('load', () => {
+                    content.style.transform = 'rotateX(-90deg)'; 
+                });
+            }
+        }
+    });
+});
